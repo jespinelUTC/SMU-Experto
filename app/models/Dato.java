bd9dd5b091b23f010fc245a40390b494d08f7414
renequisaguano <renequisaguano@gmail.com>
2014-12-01 17:15:05 -0500

    Disenando modulo de datos del analisis

--- app/models/Dato.java
+++ app/models/Dato.java
@@ -9,6 +9,21 @@ import java.util.*;
 @Entity
 public class Dato extends Model {
 	public Date fecha;
+	public String plataforma;
+	public String agenteUsuario;
+	public String url;
+	public String nombreSitio;
+	
+	//Constructor
+	public Dato(Date f, String p,String a,String url,String n){
+		this.fecha=f;
+		this.plataforma=p;
+		this.agenteUsuario=a;
+		this.url=url;
+		this.nombreSitio=n;
+		
+	}
+	
 	
     
 }
