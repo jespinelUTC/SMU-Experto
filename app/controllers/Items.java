dbe2c2af4d55a2fd08149113052479a4220d1932
edgar-ach <edgar.acurio@gmail.com>
2014-12-01 17:08:34 -0500

    Enlaces a Creacion de items

--- app/controllers/Items.java
+++ app/controllers/Items.java
@@ -1,11 +1,26 @@
 package controllers;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import models.Item;
+import models.Parametro;
+import play.db.jpa.JPA;
 import play.mvc.*;
 
 public class Items extends Controller {
-
-    public static void index() {
-        render();
+	
+    public static void index(Long idParametro ) {
+    	Query query = JPA.em().createQuery("select * from item where parametro_id="+idParametro);
+        Parametro par= Parametro.findById(idParametro);
+    	//User.find(“byUsername”, usr).first();
+    	List <Item> it = query.getResultList();
+    	System.out.print("La consulta"+it);
+        render(par);
     }
+    public static void crearItem(Long idItem) {
+		render(idItem);
+	}
 
 }
