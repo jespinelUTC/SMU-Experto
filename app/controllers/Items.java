0f99968e5e83248868c7864e1d57e9aab7fb6038
edgar-ach <chirix27@gmail.com>
2014-12-17 17:07:03 -0500

    Eliminacion implementata en Parametros e Items

--- app/controllers/Items.java
+++ app/controllers/Items.java
@@ -2,11 +2,8 @@ package controllers;
 
 import java.util.List;
 
-import javax.persistence.Query;
-
 import models.Item;
 import models.Parametro;
-import play.db.jpa.JPA;
 import play.mvc.*;
 
 public class Items extends Controller {
@@ -14,12 +11,20 @@ public class Items extends Controller {
     public static void index(Long idParametro ) {
     	Parametro par= Parametro.findById(idParametro);
     	//User.find(“byUsername”, usr).first();
-    	
-    	List <Item> it= Item.find(" parametro_id= ?", idParametro).fetch();   
-        render(par,it);
+    	//List<Item> itms =Item.findAll();    	
+    	List <Item> itms= Item.find("parametro_id= ?", idParametro).fetch();    	
+        render(par,itms);
     }
-    public static void crearItem(Long idItem) {
-		render(idItem);
+    public static void crearItem(Long idParam,String pregunta) {
+    	Parametro p =  Parametro.findById(idParam); 
+    	Item nuevo = new Item(pregunta, p);
+    	nuevo.save();
+		index(idParam);
 	}
-
+    public static void eliminarItem(Long idItem) {
+		Item borrado= Item.findById(idItem); 
+		Long aux = borrado.parametro.id;
+		borrado.delete();
+		index(aux);
+	}
 }
--- app/controllers/Parametros.java
+++ app/controllers/Parametros.java
@@ -16,9 +16,23 @@ public class Parametros extends Controller {
      */
     public static void nuevoParametro(String nombre, String informacion) {
 		Parametro par = new Parametro(nombre, informacion);
-		par.save();
-		
-		
+		par.save();		
 		index();
+	}
+    public static void eliminarParametro(Long idParam) {
+		Parametro eliminar = Parametro.findById(idParam);
+		String msg;
+		try {
+			eliminar.delete();
+			msg="Se elimino correctamente";
+		} catch (Exception e) {
+			e.printStackTrace();
+			msg="No se ha podido eliminar";
+		}		
+		index(msg);
+	}
+    public static void index(String msg) {
+    	List<Parametro> par =Parametro.findAll();
+		render(msg,par);
 	}
 }
