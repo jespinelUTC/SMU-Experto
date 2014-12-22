8243a9e4a1be85689f8c5974dba21c08a20a81ae
renequisaguano <renequisaguano@gmail.com>
2014-12-21 21:43:29 -0500

    Conectando al crear cuenta iniciacndo con la validacion de usuarios

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -11,10 +11,25 @@ public class Security extends Securing.Security {
         return user != null && user.password.equals(password);
     }
 	
-	public static void guardarUsuario(String email, String nombre,String apellido,String password){
+	public static void guardarUsuario(String email, String nombre,String apellido,String password) throws Throwable{
+		
 		Usuario u=new Usuario(email,nombre,apellido,password);
 		u.save();
-		redirect("/login");
+		Securing.authenticate(email, password,true);
+		redirect("/evaluaciones");
+	}
+	
+	
+	//error para email existente
+	public static void error1(String mail){
+		   Usuario user = Usuario.find("byEmail", mail).first();
+		   String mensaje;
+		   if (user != null){
+			   mensaje="ERROR: EMAIL YA UTILIZADO";  
+			  render(mensaje);
+		   }else{
+			   render();
+		   }
 	}
 
 }
